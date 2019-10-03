226df349a8113022d26066a3fe5a69ff5fd01724
YuhangXiong <yxiong4@ualberta.ca>
2019-10-03 12:33:23 -0600

    add create video form

--- videos/views.py
+++ videos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .models import Videos
 
 
 def home(request):
@@ -12,3 +13,13 @@ class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
+
+# def create_video(request):
+#     if request.method == 'POST':
+
+
+class CreateVideos(generic.CreateView):
+    model = Videos
+    fields = ['title']
+    template_name = 'videos/create_videos.html'
+    success_url = reverse_lazy('home')
